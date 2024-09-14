6150d8c36d2effe372b240da59750ab95fccb50b
zySail <1351574385@qq.com>
2024-09-14 20:19:57 +0800

    Lab Page Table: Speed up system calls

--- kernel/proc.c
+++ kernel/proc.c
@@ -127,6 +127,17 @@ found:
     return 0;
   }
 
+  // Allocate a user/kernel shared page and initalize struct usyscall
+  if((p->sharedData = (struct usyscall *)kalloc()) == 0){
+    freeproc(p);
+    release(&p->lock);
+    return 0;
+  }
+  else{
+    //store pid
+    p->sharedData->pid = p->pid;
+  }
+
   // An empty user page table.
   p->pagetable = proc_pagetable(p);
   if(p->pagetable == 0){
@@ -153,6 +164,9 @@ freeproc(struct proc *p)
   if(p->trapframe)
     kfree((void*)p->trapframe);
   p->trapframe = 0;
+  if(p->sharedData) // free user/kernel shared page
+    kfree((void*)p->sharedData);
+  p->sharedData = 0;
   if(p->pagetable)
     proc_freepagetable(p->pagetable, p->sz);
   p->pagetable = 0;
@@ -196,6 +210,15 @@ proc_pagetable(struct proc *p)
     return 0;
   }
 
+  // map USYSCALL with struct usyscall's pa 
+  if(mappages(pagetable, USYSCALL, PGSIZE, 
+              (uint64)(p->sharedData), PTE_R | PTE_U) < 0){
+    uvmunmap(pagetable, TRAMPOLINE, 1, 0);
+    uvmunmap(pagetable, TRAPFRAME, 1, 0);
+    uvmfree(pagetable, 0);
+    return 0;
+  }
+
   return pagetable;
 }
 
@@ -206,6 +229,7 @@ proc_freepagetable(pagetable_t pagetable, uint64 sz)
 {
   uvmunmap(pagetable, TRAMPOLINE, 1, 0);
   uvmunmap(pagetable, TRAPFRAME, 1, 0);
+  uvmunmap(pagetable, USYSCALL, 1, 0);
   uvmfree(pagetable, sz);
 }
 
