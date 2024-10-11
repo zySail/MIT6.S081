3fd4602ed3fa74adf62624379380755aa2600da8
zySail <1351574385@qq.com>
2024-10-11 20:40:22 +0800

    Change to bi-directional linked lists, add linked list functions

--- kernel/bio.c
+++ kernel/bio.c
@@ -28,29 +28,71 @@
 /*
 define head: "struct buf hash_table[NR_HASH];" will be more convenient
 but head buf will cost a lot of space due to buf's "uchar data[BSIZE]";
-not define head buf need to check the first buf when looking for LRU_buf
-specifically design a head without data is complicated and unreadable
-another plan is to design a struct only cotain buf *next , buf *pointer
-and store the struct in hashtable
 */
+
+struct hash_table{
+  struct spinlock bucket_lock[NR_HASH];
+  struct buf *bucket[NR_HASH];
+};
+
 struct {
   struct spinlock lock;
   struct buf buf[NBUF];
-  struct spinlock hash_bucket_lock[NR_HASH];
-  struct buf *hash_table[NR_HASH];
+  struct hash_table hashtbl;
 } bcache;
 
+// hash_table's functions
+// --------------------------------------------------------------
 uint64 hash(uint dev, uint blockno){
   return (dev ^ blockno) % NR_HASH;
 }
+
+// insert buf node b into hash bucket[key]
+void insert(uint64 key, struct buf *b){
+  if(!b || key >= NR_HASH )
+    panic("insert");
+
+  if(!bcache.hashtbl.bucket[key]){ // bucket has no buf node
+    b->next = 0;
+    b->prev = 0;
+    bcache.hashtbl.bucket[key] = b;
+  }
+  else{
+    b->next = bcache.hashtbl.bucket[key];
+    b->prev = 0;
+    bcache.hashtbl.bucket[key]->prev = b;
+    bcache.hashtbl.bucket[key] = b;
+  }
+}
+
+// delete buf node b from hash bucket[key]
+void delete(uint64 key, struct buf *b){
+  if(!b || key >= NR_HASH )
+    panic("delete");
+
+  if(b->prev == 0){ // b is the first buf node
+    if(b->next)
+      b->next->prev = 0;
+    bcache.hashtbl.bucket[key] = b->next;
+    b->next = 0;
+  }
+  else{
+    if(b->next) 
+      b->next->prev = b->prev;
+    b->prev->next = b->next;
+    b->next = 0;
+    b->prev = 0;
+  }
+}
+//-----------------------------------------------------------
 
 void
 binit(void)
 {
   initlock(&bcache.lock, "bcache");
   for(int i = 0; i < NR_HASH; i++){
-    initlock(&bcache.hash_bucket_lock[i], "bcache");
-    bcache.hash_table[i] = 0;
+    initlock(&bcache.hashtbl.bucket_lock[i], "bcache");
+    bcache.hashtbl.bucket[i] = 0;
   }
 }
 
@@ -61,6 +103,10 @@ static struct buf*
 bget(uint dev, uint blockno)
 {
   struct buf *b;
+
+  uint64 lastest_time = 0;
+  struct buf *LRU_buf;
+  uint64 LRU_buf_key;
 
   uint64 key = hash(dev, blockno);
   acquire(&bcache.hash_bucket_lock[key]); // get bucket lock
@@ -86,14 +132,13 @@ bget(uint dev, uint blockno)
   release(&bcache.hash_bucket_lock[key]);
   acquire(&bcache.lock);
   acquire(&bcache.hash_bucket_lock[key]);
-  uint64 lastest_time = 0;
-  struct buf *LRU_buf;
-  uint64 LRU_buf_key;
+  
   // iterate over buf[], look for LRU_buf
-  for(b = bcache.buf; b != &bcache.buf[NBUF]; b++){
-    if(b->refcnt == 0) {
-      if(b->last_used_time > lastest_time){
-        LRU_buf = b;
+  for(int i = 0; i < NBUF; i++){
+    if(bcache.buf[i].refcnt == 0) {
+      if(bcache.buf[i].last_used_time > lastest_time){
+        LRU_buf = &bcache.buf[i];
+        lastest_time = bcache.buf[i].last_used_time;
       }
     }
   }
@@ -104,14 +149,15 @@ bget(uint dev, uint blockno)
   LRU_buf_key = hash(LRU_buf->dev, LRU_buf->blockno);
   acquire(&bcache.hash_bucket_lock[LRU_buf_key]);
   if(bcache.hash_table[LRU_buf_key] == LRU_buf){ // check if the head is the target
-    b = bcache.hash_table[LRU_buf_key];
     bcache.hash_table[LRU_buf_key] = LRU_buf->next;
   }
   else{
-    for(b = bcache.hash_table[LRU_buf_key]; b != 0; b++){
+    b = bcache.hash_table[LRU_buf_key];
+    while(b != 0){
         if(b->next == LRU_buf){
           b->next = LRU_buf->next;
         }
+        b++;
       }
   }
   
@@ -166,7 +212,8 @@ brelse(struct buf *b)
   acquire(&tickslock);
   b->last_used_time = ticks; 
   release(&tickslock);
-  releasesleep(&b->lock);
+
+  releasesleep(&b->lock); // release buf b's sleeplock
 
   acquire(&bcache.lock);
   b->refcnt--;
@@ -179,7 +226,6 @@ brelse(struct buf *b)
     bcache.head.next->prev = b;
     bcache.head.next = b;
   }
-  
   release(&bcache.lock);
 }
 
