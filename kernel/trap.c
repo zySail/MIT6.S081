44fa0bf7f34f3f4d77142d24ff15d64d21d373f3
zySail <1351574385@qq.com>
2024-11-11 17:44:32 +0800

    add page fault detect

--- kernel/trap.c
+++ kernel/trap.c
@@ -6,6 +6,8 @@
 #include "proc.h"
 #include "defs.h"
 
+// #include "file.h"
+
 struct spinlock tickslock;
 uint ticks;
 
@@ -28,6 +30,59 @@ trapinithart(void)
 {
   w_stvec((uint64)kernelvec);
 }
+
+// int handle_page_fault(uint64 va){
+//   struct proc *p = myproc();
+//   struct VMA *vp;
+//   int i;
+//   uint64 pa;
+//   int perm;
+//   struct inode *ip;
+//   uint off;
+//   int prot;
+
+//   if((va % PGSIZE) != 0)
+//     panic("uvmunmap: not aligned");
+
+//   if((vp = getVMA(va)) < 0)
+//     return -1;
+  
+//   ip = vp->fp->ip;
+//   off = vp->offest;
+//   prot = vp->prot;
+
+//   // alloc a new physical page
+//   if((pa = (uint64)kalloc()) == 0){
+//     printf("no mem for a new page\n");
+//     return -1;
+//   }
+//   memset((char*)pa, 0, PGSIZE);
+
+//   // read one page from file into pa
+//   begin_op();
+//   ilock(ip);
+//   if(readi(ip, 0, pa, off, PGSIZE) < 0){
+//     iunlock(ip);
+//     kfree((void*)pa);
+//     return -1;
+//   }
+//   iunlock(ip);
+//   end_op();
+//   p->VMAs[i].off += PGSIZE; // update VMA off
+
+//   // map page
+//   perm = 0;
+//   if(prot & PROT_READ)
+//     perm |= PTE_R;
+//   if(prot & PROT_WRITE)
+//     perm |= PTE_W;
+//   if(prot & PROT_EXEC)
+//     perm |= PTE_X;
+    
+//   mappages(p->pagetable, va, PGSIZE, pa, perm | PTE_U);
+  
+//   return 0;
+// }
 
 //
 // handle an interrupt, exception, or system call from user space.
@@ -65,7 +120,16 @@ usertrap(void)
     intr_on();
 
     syscall();
-  } else if((which_dev = devintr()) != 0){
+  }
+  // else if(r_scause() == 0xd){
+  //   if(p->killed)
+  //     exit(-1);
+
+  //   uint64 fault_va = r_stval();
+  //   if(handle_page_fault(PGROUNDDOWN(fault_va)) < 0)
+  //     p->killed = 1;
+  // } 
+  else if((which_dev = devintr()) != 0){
     // ok
   } else {
     printf("usertrap(): unexpected scause %p pid=%d\n", r_scause(), p->pid);
