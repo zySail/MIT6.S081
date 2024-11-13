0172ceb36cb959ac0b56debc510e74dc3eccd0b9
zySail <1351574385@qq.com>
2024-11-13 13:00:58 +0800

    add write_vma_back

--- kernel/defs.h
+++ kernel/defs.h
@@ -33,6 +33,7 @@ void            fileinit(void);
 int             fileread(struct file*, uint64, int n);
 int             filestat(struct file*, uint64 addr);
 int             filewrite(struct file*, uint64, int n);
+int             write_vma_back(uint64 va);
 
 // fs.c
 void            fsinit(int);
